1e814061a59146db35b4280af6d05d0b36f806f2
johnNaduer <naduer789@gmail.com>
2023-05-03 20:38:28 -0500

    feat: update

---
+++ orm_practice/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from sqlalchemy import Column, Integer, String
+
+""" importamos declarative_base para preparar el molde """
+from sqlalchemy.ext.declarative import declarative_base
+
+
+"""creo el molde para crear una tabla"""
+Base = declarative_base()
+
+class state(Base):
+    __tablename__ = 'state'
+    """ nullable quiere decir que no debe tenr campos vacios"""
+    id = Column(Integer, primary_key = True, autoincrement = True, nullable = False)
+    name = Column(String(128), nullable = False)
